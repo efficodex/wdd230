1ccc49a8cd56ae68d2dd236b0be52809bd826c68
efficodex <alexandraruizfernandez01@gmail.com>
2023-11-11 22:12:02 -0500

    chamber page

--- chamber/scripts/home-page.js
+++ chamber/scripts/home-page.js
@@ -1,3 +1,12 @@
+const hamButton = document.querySelector('#hamburgerBtn');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
 var lastModified = document.querySelector('#lastModified');
 var dateLastModified = new Date(document.lastModified);
 
